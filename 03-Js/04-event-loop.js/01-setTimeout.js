955c1dae14629d76215538e6555d42429574ae7b
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-06-30 20:18:41 +0530

    Most Interview - setTimeout

---
+++ 03-Js/04-event-loop.js/01-setTimeout.js
@@ -0,0 +1,11 @@
+/**
+ * setTimeout :- requests runtime to run the function(f)
+ *  after 5 seconds 
+ *   
+ *  setTimeout : ( Asynchronous / Non Blocking )
+ */
+setTimeout(() => {
+    console.log("function is called ..!");
+}, 3000);
+
+console.log("End Line");
---
+++ 03-Js/04-event-loop.js/02-setTimeout.js
@@ -0,0 +1,13 @@
+console.log(1 );
+
+setTimeout(() => {
+    console.log(2);
+}, 3000);
+
+console.log(3);
+
+setTimeout(() => {
+    console.log(4);
+}, 6000);
+
+console.log(5);
